277ab0fd89d4ec557a5ea9322d6209c1e96888d8
USERVT <luciasara2304@gmail.com>
2026-09-03 16:28:39 -0500

    Vacio y tamaño

--- src/MiListaDoble.java
+++ src/MiListaDoble.java
@@ -1,2 +1,84 @@
-public class MiListaDoble {
+public class MiListaDoble implements ListInterface {
+
+    protected DoubleNode cabeza;
+    protected DoubleNode cola;
+
+    @Override
+    public boolean isEmpty() {
+        return this.cabeza == null;
+    }
+
+    @Override
+    public int getSize() {
+        int contador = 0;
+        DoubleNode iterador = this.cabeza;
+        while (iterador != null) {
+            contador++;
+            iterador = iterador.siguiente;
+        }
+        return contador;
+    }
+
+    @Override
+    public void clear() { }
+
+    @Override
+    public Object getHead() { return null; }
+
+    @Override
+    public Object getTail() { return null; }
+
+    @Override
+    public Object get(DoubleNode node) { return null; }
+
+    @Override
+    public DoubleNode search(Object object) { return null; }
+
+    @Override
+    public boolean add(Object object) { return false; }
+
+    @Override
+    public boolean insert(DoubleNode node, Object object) { return false; }
+
+    @Override
+    public boolean insert(Object objectRef, Object object) { return false; }
+
+    @Override
+    public boolean insertHead(Object object) { return false; }
+
+    @Override
+    public boolean insertTail(Object object) { return false; }
+
+    @Override
+    public boolean set(DoubleNode node, Object object) { return false; }
+
+    @Override
+    public boolean remove(DoubleNode node) { return false; }
+
+    // --- ESQUELETOS DE OPERACIONES DE EXPANSIÓN ---
+
+    @Override
+    public boolean contains(Object object) {
+        return false;
+    }
+
+    @Override
+    public Object[] toArray() {
+        return new Object[0];
+    }
+
+    @Override
+    public Object[] toArray(Object[] object) {
+        return new Object[0];
+    }
+
+    @Override
+    public MiListaDoble subList(DoubleNode from, DoubleNode to) {
+        return null;
+    }
+
+    @Override
+    public MiListaDoble sortList() {
+        return null;
+    }
 }
